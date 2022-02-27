ef1dd29d82f0113be60941c19715b1ecfbbd81e9
anamahmad03 <git2016Hub!>
2022-02-27 16:46:22 -0500

    Added graphs and alternative algorithms

---
+++ RunApp.py
@@ -0,0 +1,72 @@
+from SpamOrHamModel import st, msg_todisplay, getBowTransformer, getTFIDF, predict_spam_ham
+
+menu = ["Home", "Data Exploration", "Prediction: Spam or Ham"]
+choice = st.sidebar.selectbox('Navigation', menu)
+st.sidebar.markdown("Please use drop down to navigate to different pages")
+
+if choice == 'Home':
+    st.title('CSDA 1040 Group 2:')
+    st.title('Spam Detection Model')
+    st.header(
+        "In this project, our model determines if an sms is spam or not. We are using Natual Language Processing and the Multinomial Naive Bayes algorithm." +
+        "We used the ham and spam message data from")
+    st.subheader("https://archive.ics.uci.edu/ml/datasets/sms+spam+collection")
+
+    st.subheader("Lakshmi Sameera Vemula")
+    st.subheader("Cheng Qian")
+    st.subheader("Joey Santiago")
+    st.subheader("Nareshini Dookhy")
+    st.subheader("Fatima Anam Ahmad")
+
+
+
+elif choice == 'Prediction: Spam or Ham':
+
+    st.title('Verifying SMS is spam or not')
+
+    message_selected = st.text_input("Please enter a message:")
+    algorithm_selected=st.selectbox("Select an algorithm:", ['SVC', 'Naive Bayes', 'KNN', 'Decision Tree', 'Logistic Regression', 'Random Forest'])
+
+    if st.button('Ham or Spam?'):
+        st.write("Your message: ", message_selected)
+        st.write("Your algorithm: ", algorithm_selected)
+        prediction = predict_spam_ham([message_selected], algorithm_selected)
+
+        col1, col2 = st.columns([8, 6])
+
+        if (prediction.flat[0] == 'ham'):
+            with col1:
+                st.markdown('### Ham! Looks like the message is good!')
+
+            with col2:
+                st.image('.\safe.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB",
+                         output_format="auto")
+        else:
+
+            with col1:
+                st.markdown('### Spam! Looks like spam. Be careful!')
+
+            with col2:
+                st.image('.\warning.png', caption=None, width=None, use_column_width=None, clamp=False, channels="RGB",
+                         output_format="auto")
+
+else:
+    st.title("Data Exploration")
+    menu_list = ['Ham / Spam Word Freq by Size', 'Proportion of Ham vs Spam', 'Message length by Ham vs Spam']
+    menu = st.radio("Menu", menu_list)
+
+    if menu == 'Ham / Spam Word Freq by Size':
+        st.header('Ham Word Frequency')
+        st.image('.\Visualizations\ham freq - size.png')
+        st.header('Spam Word Frequency')
+        st.image('.\Visualizations\spam freq - size.png')
+
+    elif menu == 'Proportion of Ham vs Spam':
+        st.header('Pie Chart: Ham vs. Spam')
+        st.image('.\Visualizations\pie chart.png')
+        st.header('Bar Chart: Ham vs Spam')
+        st.image('.\Visualizations\histgram.png')
+
+    else:
+        st.header('Ham vs. Spam Message length')
+        st.image('.\Visualizations\ham spam msg len.png')
---
+++ SpamOrHamModel.py
@@ -0,0 +1,106 @@
+import streamlit as st
+import pickle
+import pandas as pd, numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+message_list = pickle.load(open('.\message_list.pkl', 'rb'))
+spamlabel_list = pickle.load(open('.\spamlabel_list.pkl', 'rb'))
+msg_todisplay = pd.DataFrame(list(zip(message_list, spamlabel_list)), columns=['message', 'spam_label'])
+
+
+# Count vectorizer for bag of words
+
+# Bag of words transformer
+def getBowTransformer(mgs):
+    cv = CountVectorizer()
+    bow_transformer = cv.fit(mgs)
+    return bow_transformer
+
+
+# vecortizer
+def getTFIDF(msg_bow):
+    tfidf_transformer = TfidfTransformer().fit(msg_bow)
+    messages_tfidf = tfidf_transformer.transform(msg_bow)
+    return messages_tfidf
+
+
+# the training and test sets have to have the same shape
+# the shape is determined when transforming
+# append the new message to the end of messages
+# transform data
+# return the last row of transformed data as thatrepresents the test case
+def transform_train_test(msg):
+    ser_msg = msg_todisplay['message']
+
+    ser_new_msg = pd.Series(msg)
+    print("shape new msg: ", ser_new_msg.shape)
+
+    ser_msg = ser_msg.append(ser_new_msg)
+    print("Initial shape: ", ser_msg.shape)
+
+    train_bow_transformer = getBowTransformer(ser_msg)
+    train_messages_bow = train_bow_transformer.transform(ser_msg)
+    train_messages_tfidf = getTFIDF(train_messages_bow)
+
+    print("Resulting shape: ", train_messages_tfidf.shape)
+    print(type(train_messages_tfidf))
+
+    #last_row = len(ser_msg) - 1
+    #test_row = train_messages_tfidf.getrow(last_row)
+    test_row = train_messages_tfidf[-1:]
+
+    train_messages_tfidf = train_messages_tfidf[0:-1]
+
+    print('resulting test shape', test_row.shape)
+    print('resutling train shape:', train_messages_tfidf.shape)
+
+    return test_row, train_messages_tfidf
+
+
+# returns new multinomial naive bayes model
+# after training
+def training(train, alogrithm, label_results):
+    if alogrithm == 'SVC':
+        model = SVC(kernel='sigmoid', gamma=1.0).fit(train, label_results)
+    elif alogrithm == 'KNN':
+        model = KNeighborsClassifier(n_neighbors=49).fit(train, label_results)
+    elif alogrithm == 'Decision Tree':
+        model = DecisionTreeClassifier(min_samples_split=7, random_state=111).fit(train, label_results)
+    elif alogrithm == 'Logistic Regression':
+        model = LogisticRegression(solver='liblinear', penalty='l1').fit(train, label_results)
+    elif alogrithm == 'Random Forest':
+        model = RandomForestClassifier(n_estimators=31, random_state=111).fit(train, label_results)
+    else:
+        model = MultinomialNB().fit(train, label_results)
+    return model
+
+
+# get the model that uses the message data
+def get_model(vectorized_train_data, algorithm):
+
+    msg_train, msg_test, label_train, label_test = train_test_split(vectorized_train_data, msg_todisplay['spam_label'],
+                                                                    test_size=0.25)
+
+    spam_detect_model = training(msg_train, algorithm, label_train)
+
+    return spam_detect_model
+
+
+# predict if a message is ham or spam
+def predict_spam_ham(msg, algorithm):
+
+    test_msg, train_data = transform_train_test(msg)
+
+    model = get_model(train_data, algorithm)
+
+    prediction = model.predict(test_msg)
+
+    return prediction
